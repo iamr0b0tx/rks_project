2a09f65033c5ebb67ad6603d8c72ef0ddff77a58
Joshua Christian <joshua3christianx@gmail.com>
2020-04-11 17:11:53 +0100

    first milestone reached! can collect input and retrieve long and lat from map

--- static/js/functions.js
+++ static/js/functions.js
@@ -2,12 +2,16 @@ function toggle_form_state(element){
     if(element.id == 'query_btn'){
         query_btn.style.opacity = 1.0;
         upload_btn.style.opacity = 0.5;
+        submit_btn.innerHTML = "Predict";
+        value_input.setAttribute("disabled", "disabled");
         return
     }
 
     if (element.id == 'upload_btn'){
         query_btn.style.opacity = 0.5;
         upload_btn.style.opacity = 1.0;
+        submit_btn.innerHTML = "Upload";
+        value_input.removeAttribute("disabled");
         return
     }
 }
--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,17 @@
-// mapbox code
+// mapbox code initializations
 mapboxgl.accessToken = 'pk.eyJ1IjoiaWFtcjBiMHR4IiwiYSI6ImNrOHJtdm5pbTA0MHUzbHBqZ292ODdnYjAifQ.NkW4bdn3I8VXvbP7-IeE5w';
 var coordinates = document.getElementById('coordinates');
+
+// code initializations
+var value_input = document.getElementById('value_input');
+
+var query_btn = document.getElementById('query_btn');
+var submit_btn = document.getElementById('submit_btn');
+var upload_btn = document.getElementById('upload_btn');
+
+var longitude = document.getElementById('longitude');
+var latitude = document.getElementById('latitude');
+
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
@@ -17,12 +28,11 @@ var marker = new mapboxgl.Marker({
 function onDragEnd() {
     var lngLat = marker.getLngLat();
     coordinates.style.display = 'block';
-    coordinates.innerHTML =
-        'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
+    coordinates.innerHTML = 'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
+
+    // update input field
+    longitude.value = lngLat.lng;
+    latitude.value = lngLat.lat;
 }
 
 marker.on('dragend', onDragEnd);
-
-// custom code
-var query_btn = document.getElementById('query_btn');
-var upload_btn = document.getElementById('upload_btn');
