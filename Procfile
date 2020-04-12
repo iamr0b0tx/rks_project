6cbd763c6f42d5d233aef55902f2ac483916b61f
Joshua Christian <joshua3christianx@gmail.com>
2020-04-12 16:10:05 +0100

    preping for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn rks_project.wsgi --log-file -
--- rks_project/settings.py
+++ rks_project/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
