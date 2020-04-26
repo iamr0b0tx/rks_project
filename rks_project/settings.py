063b20e08601ff64c0fc6d6a0f1db6f63b733fea
Joshua Christian <joshua3christianx@gmail.com>
2020-04-26 16:24:46 +0100

    preping for heroku deploy

--- rks_project/settings.py
+++ rks_project/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "app/static")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "frontend/static"),
     # os.path.join(BASE_DIR, "property/static"),
