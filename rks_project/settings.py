2838d9891b33db2846a3377c8aed324c50f990b9
Joshua Christian <joshua3christianx@gmail.com>
2020-04-26 01:33:22 +0100

    preping for heroku deploy

--- rks_project/settings.py
+++ rks_project/settings.py
@@ -154,9 +154,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, "static"),
+    os.path.join(BASE_DIR, "frontend/static"),
+    os.path.join(BASE_DIR, "property/static"),
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
